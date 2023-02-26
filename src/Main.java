76a35b4938f9f197f696e2048fcbfbed584f15b5
Evgeniya Yarotskaya <evgeniya047@mail.ru>
2023-02-25 18:55:22 -0800

    Change name

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 200; // Сумма на счету клиента
-        int b = 1500; // Сумма пополнения счета
+        int account = 200; // Сумма на счету клиента
+        int payment = 1500; // Сумма пополнения счета
 
         int bonus;  // Количество положенных бонусов
-        if(b > 1000) {
-            bonus = b / 100 * 1;
+        if(payment > 1000) {
+            bonus = payment / 100 * 1;
         } else {
             bonus = 0;
         }
 
-        /* int bonus = b > 1000 ? b / 100 * 1 : 0; */
+        /* int bonus = payment > 1000 ? payment / 100 * 1 : 0; */
 
-        int amount = a + b + bonus; // Итоговая сумма на счету клиента
+        int amount = account + payment + bonus; // Итоговая сумма на счету клиента
 
         System.out.println("Сумма на счету: " + amount);
         System.out.println("Количество бонусов: " + bonus);
